994e08810dbb3aea013aaaefb566b875c66e73a8
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-12-02 11:06:41 +0100

    chore.refactor(arduino-project): create new folder

---
+++ arduino-project/tests/CMakeFiles/executeTests.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jp/repo/robobox-02-bras-articule/arduino-project/tests/basic-test.cpp" "/home/jp/repo/robobox-02-bras-articule/arduino-project/tests/CMakeFiles/executeTests.dir/basic-test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
